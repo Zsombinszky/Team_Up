596e1942f2d6d62aca5b2b4d1e95c175a1ee066e
Balint Szollosy <szollosybalint24@gmail.com>
2024-05-17 10:12:29 +0200

    navbar links fixed

--- client/src/component/Companies.jsx
+++ client/src/component/Companies.jsx
@@ -5,8 +5,8 @@ const Companies = () => {
     return (
         <div className='w-full bg-white py-[50px]'>
             <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
-                <h1 className="text-center text-2xl font-bold text-[#536E96]">Trusted by over 25,000 teams around the world.</h1>
-                <p className="text-center text-[#536E96] text-xl">Leading companies use the same courses to help employees keep their skills fresh.</p>
+                <h1 className="text-center text-2xl font-bold text-[#536E96]">Trusted by over 25,000 gamers around the world.</h1>
+                <p className="text-center text-[#536E96] text-xl">Leading companies use TeamUp to help gamers keep their skills fresh.</p>
                 <div className="flex justify-center py-8 md:gap-8 ">
                     <img src={companyLogo1} alt='hero'/>
                     <img src={companyLogo2} alt='hero'/>
