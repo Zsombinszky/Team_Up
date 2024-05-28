54cebc407283874770b74fd94ec5ead10053eb21
Zsombor Pocs <142238433+Zsombinszky@users.noreply.github.com>
2024-05-29 00:23:06 +0200

    newsandloginpagefixed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,9 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import LoginBackground from "../assets/loginbgbig.jpg"
+import LoginBackground2 from "../assets/loginbg2.jpg"
+import LoginBackground3 from "../assets/loginbg3.jpg"
+import LoginBackground4 from "../assets/loginbg4.jpg"
 
 const LoginPage = () => {
 
@@ -40,45 +43,72 @@ const LoginPage = () => {
         }
     }
 
-
     return (
-        <div className={"w-full h-screen"}>
-            <img
-                className='hidden sm:block absolute w-full h-full object-cover'
-                src={LoginBackground}
-                alt='/'
-            />
-            <div className='fixed w-full px-4 py-24 z-50'>
-                <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
-                    <div className='max-w-[320px] mx-auto py-16'>
-                        <h1 className='text-3xl font-bold py-8'>Sign In</h1>
-                        <form onSubmit={handleSubmit} className='w-full flex flex-col'>
-                            <p><label className={"block uppercase tracking-wide text-sm font-bold mb-2"}
-                                      htmlFor="userName">Username</label></p>
+        <div className="w-full h-screen relative">
+            <div className="carousel w-full h-full absolute object-cover z-0">
+                <div id="item1" className="carousel-item w-full">
+                    <img src={LoginBackground} className="w-full"/>
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <img src={LoginBackground2} className="w-full"/>
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <img src={LoginBackground3} className="w-full"/>
+                </div>
+                <div id="item4" className="carousel-item w-full">
+                    <img src={LoginBackground4} className="w-full"/>
+                </div>
+            </div>
+            <div className="fixed bottom-4 w-full flex justify-center gap-2 z-50 mb-5">
+                <a href="#item1" className="btn btn-sm text-yellow-300 bg-black">1</a>
+                <a href="#item2" className="btn btn-sm text-yellow-300 bg-black">2</a>
+                <a href="#item3" className="btn btn-sm text-yellow-300 bg-black">3</a>
+                <a href="#item4" className="btn btn-sm text-yellow-300 bg-black">4</a>
+            </div>
+            <div className="fixed w-full px-4 py-24 z-50 items-center">
+                <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+                    <div className="max-w-[320px] mx-auto py-16">
+                        <h1 className="text-3xl font-bold pb-8">Sign In</h1>
+                        <form onSubmit={handleSubmit} className="w-full flex flex-col">
+                            <p>
+                                <label
+                                    className="block uppercase tracking-wide text-sm font-bold mb-2"
+                                    htmlFor="userName"
+                                >
+                                    Username
+                                </label>
+                            </p>
                             <input
                                 onChange={(e) => setUsername(e.target.value)}
-                                className='p-3 my-2 bg-gray-700 rounded'
+                                className="p-3 my-2 bg-gray-700 rounded"
                                 value={username}
-                                type='text'
-                                placeholder='username'
+                                type="text"
+                                placeholder="username"
                             />
-                            <p><label className={"block uppercase tracking-wide text-sm font-bold mb-2 mt-5"}
-                                      htmlFor="password">Password</label></p>
+                            <p>
+                                <label
+                                    className="block uppercase tracking-wide text-sm font-bold mb-2 mt-5"
+                                    htmlFor="password"
+                                >
+                                    Password
+                                </label>
+                            </p>
                             <input
                                 onChange={(e) => setPassword(e.target.value)}
-                                className='p-3 my-2 bg-gray-700 rounded'
-                                type='password'
+                                className="p-3 my-2 bg-gray-700 rounded"
+                                type="password"
                                 value={password}
-                                placeholder='password'
-                                autoComplete='current-password'
+                                placeholder="password"
+                                autoComplete="current-password"
                             />
                             <button
-                                className='bg-yellow-500 hover:bg-[#FFD700] py-3 my-6 rounded font-bold ease-in duration-200'>
+                                className="bg-yellow-500 hover:bg-[#FFD700] py-3 my-6 rounded font-bold ease-in duration-200"
+                            >
                                 Sign In
                             </button>
-                            <div className='flex justify-between items-center text-sm text-gray-600'>
+                            <div className="flex justify-between items-center text-sm text-gray-600">
                                 <p>
-                                    <input className='mr-2 accent-amber-400' type='checkbox'/>
+                                    <input className="mr-2 accent-amber-400" type="checkbox"/>
                                     Remember me
                                 </p>
                             </div>
@@ -87,6 +117,6 @@ const LoginPage = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 export default LoginPage
